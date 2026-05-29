86762b6eec82d90069718d0e121837269ea4696b
Akanksha-singh27 <akankshasingh2700@gmail.com>
2026-05-29 08:34:05 +0000

    data

---
+++ basics/variable.js
@@ -0,0 +1,9 @@
+const accountId=14453
+let accountEmail="akanksha@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+console.table([accountId,accountEmail,accountPassword,accountCity]);//ek sath tabular form m likh sakte
+accountEmail="hc@google.com"
+accountPassword="121212"
+accountCity="Milan"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
